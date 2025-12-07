3709688f56e09617c1e1ce16cc3c40443d5686f3
Michelle <michbetz.10@gmail.com>
2025-12-07 17:20:15 -0600

    Crea los ts propuestos para el modulo usuarios

---
+++ usuarios/s-03-tablespaces.sql
@@ -0,0 +1,34 @@
+--TS CLIENTES 
+
+CREATE BIGFILE TABLESPACE clientes_c1_data_ts
+    DATAFILE '/unam/bda/pf/c1/d01/clientes_c1_data_ts_01.dbf'
+    SIZE 5G
+    AUTOEXTEND ON NEXT 500M
+    MAXSIZE 20G;
+
+CREATE BIGFILE TABLESPACE clientes_c2_lob_ts
+    DATAFILE '/unam/bda/pf/c2/d01/clientes_c2_lob_ts_01.dbf'
+    SIZE 10G
+    AUTOEXTEND ON NEXT 1G
+    MAXSIZE 50G;
+
+CREATE BIGFILE TABLESPACE sensibles_c2_lob_ts
+    DATAFILE '/unam/bda/pf/c2/d01/clientes_c2_lob_ts_01.dbf'
+    SIZE 10G
+    AUTOEXTEND ON NEXT 1G
+    MAXSIZE 50G;
+
+
+--TS OPERACION 
+
+REATE BIGFILE TABLESPACE operacion_c1_data_ts
+    DATAFILE '/unam/bda/pf/c1/d01/operacion_c1_data_ts_01.dbf'
+    SIZE 5G
+    AUTOEXTEND ON NEXT 500M
+    MAXSIZE 20G;
+
+CREATE BIGFILE TABLESPACE operacion_c2_lob_ts
+    DATAFILE '/unam/bda/pf/c2/d01/operacion_c2_lob_ts_01.dbf'
+    SIZE 10G
+    AUTOEXTEND ON NEXT 1G
+    MAXSIZE 50G;
