0d2ec13525bdbdde0a24cff0bf83ed195df31eef
Pilar <pilarjac07@gmail.com>
2025-12-08 22:25:48 -0600

    Tablespaces

---
+++ infraestructura/s-02-rutas-tablespaces.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#@Autor(es):   Benítez Pérez Michelle Paulina
+#              Pilar Jaqueline Hernández García
+#@Fecha creación: 8/12/2025
+#@Descripción: el script crea directorios para los datafiles de los tablespaces
+
+'/unam/bda/pf/c1/d03/infra_c1_data_ts.dbf' 
+'/unam/bda/pf/c1/d04/infra_c1_ix_ts.dbf' 
+'/unam/bda/pf/c1/d05/infra_c1_lob_ts.dbf' 
+
+'/unam/bda/pf/c2/d03/infra_c2_lob_ts.dbf' 
+'/unam/bda/pf/c2/d04/infra_c2_hist_ts.dbf'
--- infraestructura/s-02-tablespaces.sql
+++ infraestructura/s-02-tablespaces.sql
@@ -3,41 +3,47 @@
 --@Fecha creación: 08/12/2025
 --@Descripción: 
 
--- 1. Tablespace para Datos (Capa 1 - Alto Desempeño)
--- Asignación propuesta: 10 GB
-CREATE TABLESPACE infraestr_c1_data_ts
-  DATAFILE '/unam/bda/pf/c1/d01/infraestr_c1_data_ts_01.dbf'
-  SIZE 100M
-  AUTOEXTEND ON NEXT 50M MAXSIZE 10G
-  EXTENT MANAGEMENT LOCAL AUTOALLOCATE
-  SEGMENT SPACE MANAGEMENT AUTO;
+-- 1. Tablespace para Datos Operativos (Capa 1)
+-- Almacena tablas como: gimnasio, sala, aparato, disciplina
+CREATE TABLESPACE infra_c1_data_ts
+    DATAFILE '/unam/bda/pf/c1/d03/infra_c1_data_ts.dbf' 
+    SIZE 1G
+    AUTOEXTEND ON NEXT 500M MAXSIZE 8G
+    EXTENT MANAGEMENT LOCAL AUTOALLOCATE
+    SEGMENT SPACE MANAGEMENT AUTO;
 
--- 2. Tablespace para Índices (Capa 1 - Alto Desempeño)
--- Asignación propuesta: 5 GB 
-CREATE TABLESPACE infraestr_c1_ix_ts
-  DATAFILE '/unam/bda/pf/c1/d02/infraestr_c1_ix_ts_01.dbf'
-  SIZE 100M
-  AUTOEXTEND ON NEXT 50M MAXSIZE 5G
-  EXTENT MANAGEMENT LOCAL AUTOALLOCATE
-  SEGMENT SPACE MANAGEMENT AUTO;
+-- 2. Tablespace para Índices (Capa 1)
+-- Almacena los índices de las tablas operativas para búsquedas rápidas
+CREATE TABLESPACE infra_c1_ix_ts
+    DATAFILE '/unam/bda/pf/c1/d04/infra_c1_ix_ts.dbf' 
+    SIZE 50M
+    AUTOEXTEND ON NEXT 50M MAXSIZE 2G
+    EXTENT MANAGEMENT LOCAL AUTOALLOCATE
+    SEGMENT SPACE MANAGEMENT AUTO;
 
--- 3. Tablespace para objetos LOB (Capa 2 - Costo Medio/Almacenamiento Masivo)
--- Asignación propuesta: 10 GB (Los LOBs suelen consumir mucho espacio)
-CREATE TABLESPACE infraestr_c2_lob_ts
-  DATAFILE '/unam/bda/pf/c2/d01/infraestr_c2_lob_ts_01.dbf'
-  SIZE 100M
-  AUTOEXTEND ON NEXT 100M MAXSIZE 10G
-  EXTENT MANAGEMENT LOCAL AUTOALLOCATE
-  SEGMENT SPACE MANAGEMENT AUTO;
+-- 3. Tablespace para Multimedia (Capa 2)
+-- Almacena datos BLOB pesados (videos/imágenes) con menor frecuencia de acceso (Warn/Cold)
+CREATE TABLESPACE infra_c2_lob_ts
+    DATAFILE '/unam/bda/pf/c2/d03/infra_c2_lob_ts.dbf' 
+    SIZE 100M
+    AUTOEXTEND ON NEXT 50M MAXSIZE 5G
+    EXTENT MANAGEMENT LOCAL AUTOALLOCATE
+    SEGMENT SPACE MANAGEMENT AUTO;
 
--- 4. Tablespace para Datos Históricos (Capa 2 - Datos "Fríos")
--- Asignación ropuesta: 5 GB
--- Se recomienda compresión para datos históricos para ahorrar espacio
-CREATE TABLESPACE infraestr_c2_hist_ts
-  DATAFILE '/unam/bda/pf/c2/d02/infraestr_c2_hist_ts_01.dbf'
-  SIZE 100M
-  AUTOEXTEND ON NEXT 50M MAXSIZE 5G
-  DEFAULT ROW STORE COMPRESS ADVANCED -- Opcional: Recomendado para históricos
-  EXTENT MANAGEMENT LOCAL AUTOALLOCATE
-  SEGMENT SPACE MANAGEMENT AUTO;
+-- 4. Tablespace para Iconos (Capa 1)
+-- Almacena datos BLOB pequeños y de alto acceso (Hot Lobs)
+CREATE TABLESPACE infra_c1_lob_ts
+    DATAFILE '/unam/bda/pf/c1/d05/infra_c1_lob_ts.dbf' 
+    SIZE 10M
+    AUTOEXTEND ON NEXT 10M MAXSIZE 100M
+    EXTENT MANAGEMENT LOCAL AUTOALLOCATE
+    SEGMENT SPACE MANAGEMENT AUTO;
 
+-- 5. Tablespace para Históricos (Capa 2)
+-- Almacena datos de historial_status_aparato
+CREATE TABLESPACE infra_c2_hist_ts
+    DATAFILE '/unam/bda/pf/c2/d04/infra_c2_hist_ts.dbf' 
+    SIZE 100M
+    AUTOEXTEND ON NEXT 50M MAXSIZE 3G
+    EXTENT MANAGEMENT LOCAL AUTOALLOCATE
+    SEGMENT SPACE MANAGEMENT AUTO;
