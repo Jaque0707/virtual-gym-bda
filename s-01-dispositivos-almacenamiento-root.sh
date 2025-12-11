ef43a2d98b1a91e40f73e7104288e08d53b00a22
Michelle <michbetz.10@gmail.com>
2025-12-10 21:32:04 -0600

    Actualiza la creación de discos

--- s-01-dispositivos-almacenamiento-root.sh
+++ s-01-dispositivos-almacenamiento-root.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+#usuario: root 
+#Nivel: S.O host 
 # ======================================================
 # Script: s-01-dispositivos-almacenamiento-root.sh
 # Autor: Benítez Pérez Michelle Paulina
@@ -10,11 +11,12 @@
 mkdir -p /unam/bda/disk-images
 cd /unam/bda/disk-images
 
+# Crea en la carpeta un archivo binarios de 1G que representa los loop devices
 dd if=/dev/zero of=disk11.img bs=100M count=10
 dd if=/dev/zero of=disk12.img bs=100M count=10
 dd if=/dev/zero of=disk13.img bs=100M count=10
 
-# Verificar los archivos creados
+# Comprueba la creación de los archivos
 du -sh disk*.img
 
 # Asociar cada imagen con un loop device disponible
@@ -34,3 +36,13 @@ mkfs.ext4 disk13.img
 mkdir -p /unam/bda/pf/c0/d11
 mkdir -p /unam/bda/pf/c0/d12
 mkdir -p /unam/bda/pf/c0/d13
+
+# Para montar los loopdevices por cada uno realizar: 
+# sudo mount -o loop /dev/loop* ${UNAM_HOME}/bda/pf/c0/d1*
+# Redirigirse a /etc/fstab y agregar 
+# /unam/bda/disk-images/disk1*.img  /unam/bda/pf/c0/d1* auto loop 0 0
+# Probar y montar:
+# sudo mount -a 
+# Reiniciar
+# Comprobar que funciona:
+# df -h | grep "${UNAM_HOME}/bda/*"e
