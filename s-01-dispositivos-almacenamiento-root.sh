a88623c39720948175cbea164c3fe025384052e0
Michelle <michbetz.10@gmail.com>
2025-12-11 18:32:27 -0600

    cambia unas cosas

--- s-01-dispositivos-almacenamiento-root.sh
+++ s-01-dispositivos-almacenamiento-root.sh
@@ -1,36 +1,48 @@
 #!/bin/bash
-
+#usuario: root 
+#Nivel: S.O host 
 # ======================================================
 # Script: s-01-dispositivos-almacenamiento-root.sh
 # Autor: Benítez Pérez Michelle Paulina
-#        Hernandez Garcia Pilar Jaquelin
+#        Hernández García Pilar Jaqueline
 # Fecha: 05/12/2025
 # Descripción: Creación de loop devices para simular discos.
 # ======================================================
 mkdir -p /unam/bda/disk-images
 cd /unam/bda/disk-images
 
-dd if=/dev/zero of=disk1.img bs=100M count=10
-dd if=/dev/zero of=disk2.img bs=100M count=10
-dd if=/dev/zero of=disk3.img bs=100M count=10
+# Crea en la carpeta un archivo binarios de 1G que representa los loop devices
+dd if=/dev/zero of=disk11.img bs=100M count=10
+dd if=/dev/zero of=disk12.img bs=100M count=10
+dd if=/dev/zero of=disk13.img bs=100M count=10
 
-# Verificar los archivos creados
+# Comprueba la creación de los archivos
 du -sh disk*.img
 
 # Asociar cada imagen con un loop device disponible
-losetup -fP disk1.img
-losetup -fP disk2.img
-losetup -fP disk3.img
+losetup -fP disk11.img
+losetup -fP disk12.img
+losetup -fP disk13.img
 
 # Confirmar la asociación
 losetup -a
 
 # Formatear cada imagen 
-mkfs.ext4 disk1.img
-mkfs.ext4 disk2.img
-mkfs.ext4 disk3.img
+mkfs.ext4 disk11.img
+mkfs.ext4 disk12.img
+mkfs.ext4 disk13.img
 
 # Crear directorios de montaje
-mkdir -p /unam/bda/disks/d11
-mkdir -p /unam/bda/disks/d12
-mkdir -p /unam/bda/disks/d13
+mkdir -p /unam/bda/pf/c0/d11
+mkdir -p /unam/bda/pf/c0/d12
+mkdir -p /unam/bda/pf/c0/d13
+
+# Para montar los loopdevices por cada uno realizar: 
+# sudo mount -o loop /dev/loop* ${UNAM_HOME}/bda/pf/c0/d1*
+# Redirigirse a /etc/fstab y agregar 
+# /unam/bda/disk-images/disk1*.img  /unam/bda/pf/c0/d1* auto loop 0 0
+# Probar y montar:
+# sudo mount -a 
+# Reiniciar
+# Comprobar que funciona:
+# df -h | grep "${UNAM_HOME}/bda/*"e
