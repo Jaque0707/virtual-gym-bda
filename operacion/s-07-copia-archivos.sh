bcee1a23752eb25a690cfa5dcc8331218cd18b9a
Michelle <michbetz.10@gmail.com>
2025-12-15 22:50:50 -0600

    Arregla fotos

--- operacion/s-07-copia-archivos.sh
+++ operacion/s-07-copia-archivos.sh
@@ -1,2 +1,11 @@
 cp -av /unam/bda/virtual-gym-bda/operacion/carga-inicial/img_clientes \
       /opt/oracle/oradata/FREE/operacion/carga-inicial/
+
+cp -av /unam/bda/virtual-gym-bda/operacion/carga-inicial/img_empleados \
+      /opt/oracle/oradata/FREE/operacion/carga-inicial/
+
+cp -av /unam/bda/virtual-gym-bda/operacion/carga-inicial/img_huellas \
+      /opt/oracle/oradata/FREE/operacion/carga-inicial/
+
+cp -av /unam/bda/virtual-gym-bda/operacion/carga-inicial/img_certificado\
+      /opt/oracle/oradata/FREE/operacion/carga-inicial/
